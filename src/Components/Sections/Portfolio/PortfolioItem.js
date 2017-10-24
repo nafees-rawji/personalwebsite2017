1f6f275fc7af21d6542dd8d7fbe984925f17a188
nafeesrawji <nafees.rawji@hotmail.com>
2017-10-24 16:33:26 -0400

    Added animation to portfolio

--- src/Components/Sections/Portfolio/PortfolioItem.js
+++ src/Components/Sections/Portfolio/PortfolioItem.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react';
+import Waypoint from 'react-waypoint';
+
 
 class PortfolioItem extends Component {
+    constructor() {
+        super();
+        this.state = {
+            className: "",
+            offset: 200
+        }
+    }
+
+    handleEntrance(e) {
+        // console.log("Entered", this.props.position);
+        this.setState({
+            className: "fadeIn"
+        });
+    }
+
     render() {
+        console.log(this.state.className);
+        const classNames = "portfolio-item animated " + this.state.className;
         return (
-            <div className={"portfolio-item"}>
-                <a href={this.props.url} className="overlay-container">
-                    <div className="overlay"/>
-                    <img className={"portfolio-image"} src={this.props.img} alt = {this.props.imgAlt}/>
-                </a>
-                <h3>{this.props.subheading}</h3>
-            </div>
+            <Waypoint
+                onEnter={this.handleEntrance.bind(this)}
+                topOffset={this.state.offset}
+                bottomOffset={this.state.offset}>
+                <div className={classNames}>
+                    <a href={this.props.url} className="overlay-container">
+                        <div className="overlay"/>
+                        <img className={"portfolio-image"} src={this.props.img} alt = {this.props.imgAlt}/>
+                    </a>
+                    <h3>{this.props.subheading}</h3>
+                </div>
+            </Waypoint>
         );
     }
 }
