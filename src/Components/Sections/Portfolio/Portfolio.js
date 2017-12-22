6d03004f237179a18a06047094f17a518fc40365
nafeesrawji <nafees.rawji@hotmail.com>
2017-12-22 14:36:17 -0500

    Finished shopify coding challenge

--- src/Components/Sections/Portfolio/Portfolio.js
+++ src/Components/Sections/Portfolio/Portfolio.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Section from "../../Section";
 import PortfolioItem from "./PortfolioItem";
 import liveImage from '../../../Images/Portfolio/LiveImage.png'
+import shopifyImage from '../../../Images/Portfolio/Shopify.png'
+
 import placeholder from '../../../Images/placeholder.jpg'
 
 class Portfolio extends Component {
@@ -12,12 +14,11 @@ class Portfolio extends Component {
                      id={"portfolio"} sectionHeading={"Portfolio"}>
                 <div className="portfolio-gallery">
                     <div className="column">
-                        <PortfolioItem img={liveImage} subheading="live competition 2017" imgAlt = "placeholder" url="/portfolio/live"/>
+                        <PortfolioItem img={liveImage} subheading="live competition 2017" imgAlt = "live-competition 2017" url="/portfolio/live"/>
                         <PortfolioItem img={placeholder} subheading="placeholder" imgAlt = "placeholder" url="http://google.com"/>
                     </div>
                     <div className="column">
-                        {/*<PortfolioItem img={liveImage} subheading="live competition 2017" imgAlt = "placeholder" url="http://google.com"/>*/}
-                        <PortfolioItem img={placeholder} subheading="placeholder" imgAlt = "placeholder" url="http://google.com"/>
+                        <PortfolioItem img={shopifyImage} subheading="shopify coding challenge" imgAlt = "shopify coding challenge" url="/portfolio/shopify"/>
                     </div>
                     <h3 id="construction-notice">This section is still under construction! More items will be added shortly.</h3>
                 </div>
--- src/Components/Shopify/ShopifyMainSection.js
+++ src/Components/Shopify/ShopifyMainSection.js
@@ -6,24 +6,40 @@ class ShopifyMainSection extends Component {
 
     state = {
         selectedOption: '',
+        email: '',
+        hasError: false
     };
 
-    handleChange(selectedOption) {
+    optionSelected(selectedOption) {
         this.setState({ selectedOption });
     };
 
-    render() {
+    emailChanged(e) {
+        this.setState({email: e.target.value});
+    }
 
+    submitClicked() {
+        // regex from stackoverflow (don't need to reinvent the wheel)
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (re.test(this.state.email.toLowerCase())) {
+            console.log(this.state.email);
+            this.setState({hasError: false});
+        } else {
+            this.setState({hasError: true});
+        }
+    }
+
+    render() {
         return (
             <div id="shopify-main-section">
-                <h3 id="shopify-subheading">Subscribe for free marketing tips</h3>
+                <h3 id="shopify-subheading" >Subscribe for free marketing tips</h3>
                 <div id="shopify-fields">
-                    <input className="shopify-input" placeholder="Email Address" type="email"/>
+                    <input className="shopify-input" placeholder="Email Address" type="email" value={this.state.email} onChange={this.emailChanged.bind(this)}/>
                     <Select
                         className="shopify-dropdown"
                         name="form-field-name"
                         value={this.state.selectedOption}
-                        onChange={this.handleChange.bind(this)}
+                        onChange={this.optionSelected.bind(this)}
                         searchable={false}
                         options={[
                             { value: 'one', label: 'Option One' },
@@ -37,8 +53,8 @@ class ShopifyMainSection extends Component {
                         }}
                     />
                 </div>
-                <p id="shopify-error-message">Please enter a valid email address</p>
-                <button id="shopify-submit-button">Sign Up Now</button>
+                {(this.state.hasError) ? <p id="shopify-error-message">Please enter a valid email address</p>: null}
+                <button id="shopify-submit-button" onClick={this.submitClicked.bind(this)}>Sign Up Now</button>
             </div>
         );
     }
